c760157b396b61811e48fa80b47b0c37cf572ba6
unknown <evor1995@gmail.com>
2021-01-02 11:23:39 +0200

    Completed course

---
+++ Assets/Scripts/Ledge.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ledge : MonoBehaviour
+{
+    [SerializeField] private Vector3 _handPos, _standPos;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("LedgeGrabChecker"))
+        {
+            Player player = other.GetComponentInParent<Player>();
+
+            if (player != null)
+            {
+                player.LedgeGrabbed(_handPos, this);
+            }
+        }
+    }
+    public Vector3 GetStandPos()
+    {
+        return _standPos;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _speed = 6.0f;
+    [SerializeField] private float _gravity = 0.15f;
+    [SerializeField] private float _jumpHeight = 12.0f;
+    [SerializeField] private GameObject _ledgeChecker;
+    private bool _jumping;
+    private bool _climbingUp = false;
+    private Ledge _currentLedge;
+
+    private CharacterController _controller;
+    private Vector3 _velocity;
+
+    private Animator _animator;
+
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+
+        if (_controller == null)
+        {
+            Debug.LogError("null component _controller::Player");
+        }
+
+        _animator = GetComponentInChildren<Animator>();
+
+        if (_animator == null)
+        {
+            Debug.LogError("null component _animator::Player");
+        }
+    }
+
+    void Update()
+    {
+        CalculateMovement();
+        ClimbingUpLedge();
+
+    }
+
+    private void ClimbingUpLedge()
+    {
+        if (_climbingUp == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                _animator.SetTrigger("ClimbUp");
+                _climbingUp = false;
+            }
+        }
+    }
+
+    private void CalculateMovement()
+    {
+        if (_controller.isGrounded)
+        {
+            if (_jumping == true)
+            {
+                _jumping = false;
+                _animator.SetBool("Jumping", false);
+            }
+
+            float _direction = Input.GetAxisRaw("Horizontal");
+            _jumping = false;
+            _velocity = new Vector3(_direction, 0, 0) * _speed;
+
+            if (_direction != 0)
+            {
+
+                Vector3 facing = transform.localEulerAngles;
+
+                if (_velocity.x > 0)
+                {
+                    facing.y = 0;
+                }
+                else
+                {
+                    facing.y = 180;
+                }
+
+                transform.localEulerAngles = facing;
+            }
+
+            _animator.SetFloat("Speed", Mathf.Abs(_direction));
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _velocity.y += _jumpHeight;
+                _jumping = true;
+                _animator.SetBool("Jumping", true);
+            }
+        }
+        _velocity.y -= _gravity;
+        _controller.Move(_velocity * Time.deltaTime);
+    }
+
+    public void LedgeGrabbed(Vector3 handPos, Ledge currentLedge)
+    {
+        _climbingUp = true;
+        _animator.SetBool("LedgeGrab", true);
+        _animator.SetFloat("Speed", 0);
+        _animator.SetBool("Jumping", false);
+        _controller.enabled = false;
+        transform.position = handPos;
+        _currentLedge = currentLedge;
+    }
+
+    public void ClimbUpComplete()
+    {
+        transform.position = _currentLedge.GetStandPos();
+        _animator.SetBool("LedgeGrab", false);
+        _controller.enabled = true;
+    }
+}
