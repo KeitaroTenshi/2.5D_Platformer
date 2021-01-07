eaf0eac9cfe1098ef2b90a970537f4209ac6ff58
unknown <evor1995@gmail.com>
2021-01-07 12:43:29 +0200

    added modular moving platform and turned it into a prefub

---
+++ Assets/Scripts/Platforms/Elevator.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    [SerializeField] private Transform _posA, _posB;
+    [SerializeField] private bool _canMove = true;
+    [SerializeField] private bool _atPointA;
+    [SerializeField] private float _elevatorWaitingRate = 5.0f;
+    private Elevator _elevatorParent;
+    void Start()
+    {
+        StartCoroutine(SwitchingRoutine());
+        _elevatorParent = GetComponentInParent<Elevator>();
+
+        if (_elevatorParent == null)
+        {
+            Debug.LogError("Elevator parent is null");
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (_canMove)
+        {
+            ElevatorMovement();
+        }
+    }
+
+    private void ElevatorMovement()
+    {
+        if (!_atPointA)
+        {
+            transform.position = Vector3.MoveTowards(this.transform.position, _posA.position, Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(this.transform.position, _posB.position, Time.deltaTime);
+        }
+
+        if (transform.position == _posA.position)
+        {
+            _canMove = false;
+            _atPointA = true;
+        }
+        if (transform.position == _posB.position)
+        {
+            _canMove = false;
+            _atPointA = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.parent = _elevatorParent.gameObject.transform;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.parent = null;
+        }
+    }
+
+    IEnumerator SwitchingRoutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1.0f);
+            if (_canMove == false)
+            {
+                yield return new WaitForSeconds(_elevatorWaitingRate);
+                _canMove = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Platforms/MovingPlatform.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private float _platformMovementSpeed = 2.0f;
+    [SerializeField] private Transform _pointA, _pointB;
+    private bool _atPointA = true;
+    private void FixedUpdate()
+    {
+        if (_atPointA)
+        {
+            transform.position = Vector3.MoveTowards(this.transform.position, _pointB.transform.position, (_platformMovementSpeed * Time.deltaTime));
+        }
+        else if (!_atPointA)
+        {
+            transform.position = Vector3.MoveTowards(this.transform.position, _pointA.transform.position, (_platformMovementSpeed * Time.deltaTime));
+        }
+
+        if (transform.position == _pointA.transform.position)
+        {
+            _atPointA = true;
+        }
+        if (transform.position == _pointB.transform.position)
+        {
+            _atPointA = false;
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.parent = this.transform;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.parent = null;
+        }
+    }
+}
